79a6ca2b785abe6bdae527673f09547feb43c8e1
Kev Aldasoro <andrewaldasoro@gmail.com>
2021-08-16 15:50:42 -0400

    🔨 query builder > initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { QueryBuilderConfig } from 'angular2-query-builder';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-query-builder';
+  query = {
+    condition: 'and',
+    rules: [],
+  };
+
+  config: QueryBuilderConfig = {
+    fields: {
+      name: { name: 'Name', type: 'string' },
+    },
+  };
 }
